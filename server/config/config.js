b8c9bfca96fa643cdae10c2be48c4173b6eba79e
kareemhamdy500 <kareemhamdy500@gmail.com>
2019-10-06 23:38:29 +0200

    add cloud database

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     sercretKey: '1218/asdsa-6665444-then why do this',
-    dbURL:'mongodb://localhost/notArabic'
+    dbURL: process.env.MONGOLAP_URI || 'mongodb://localhost/notArabic'
 }
