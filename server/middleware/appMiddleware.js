cc573fca6436bfba4e4bc1ab232b78193407f5e2
kareemhamdy500 <kareemhamdy500@gmail.com>
2019-10-08 03:22:52 +0200

    add cors

--- server/middleware/appMiddleware.js
+++ server/middleware/appMiddleware.js
@@ -1,6 +1,7 @@
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+var cors = require('cors')
 // setup global middleware here
 
 module.exports = function(app) {
@@ -8,4 +9,5 @@ module.exports = function(app) {
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
   app.use(passport.initialize());
+  app.use(cors());
 };
